d2bd696394b3e6e7f487bcb96aa1c3fe2935becc
Chanyoung Park <cypark1516@gmail.com>
2026-02-05 13:39:52 -0800

    vec3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod color;
+mod vec3;
+ 
+
+use std::io;
+ 
+use color::Color;
+
 fn main() {
     // Image
  
@@ -9,16 +17,15 @@ fn main() {
     print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
  
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
             let b = 0.25;
  
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
- 
-            print!("{} {} {}\n", ir, ig, ib);
+            let pixel_color = Color::new(r, g, b);
+            color::write_color(&mut io::stdout(), pixel_color);
         }
     }
+    eprint!("\nDone.\n");
 }
